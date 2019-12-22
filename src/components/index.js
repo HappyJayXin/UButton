68abcc7fc13867cf2f1c2ea3c9fa09b16d9e747a
HappyJayXin <36494560+HappyJayXin@users.noreply.github.com>
2019-12-22 12:40:28 +0800

    Modify: Set components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,23 @@
 import UButton from "@/components/UButton";
 
-UButton.install = function(Vue) {
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
   Vue.component("u-button", UButton);
+}
+
+const plugin = {
+  install
 };
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
 
 export default UButton;
